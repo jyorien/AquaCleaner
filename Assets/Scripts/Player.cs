2957f6cd43684809e936b4297db22aab36c2ceb0
Joey Lim <jyorin0206@gmail.com>
2022-11-06 15:16:50 +0800

    Refactored code

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -7,8 +7,9 @@ public class Player : MonoBehaviour
 {
     [SerializeField] float MovementSpeed;
     Rigidbody2D rb;
-    float horizontalMovement;
-    float verticalMovement;
+    float HorizontalMovement;
+    float VerticalMovement;
+    float PlayerRadius = 1;
 
     [SerializeField] TMP_Text scoreText;
 
@@ -21,23 +22,49 @@ public class Player : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        horizontalMovement = Input.GetAxisRaw("Horizontal");
-        verticalMovement = Input.GetAxisRaw("Vertical");
+        HorizontalMovement = Input.GetAxisRaw("Horizontal");
+        VerticalMovement = Input.GetAxisRaw("Vertical");
     }
 
     private void FixedUpdate()
     {
-        //DetectTrash(GetPlayerCenter(), 1);
+        DetectTrash(GetPlayerCenter(), PlayerRadius);
+        MovePlayer();
+    }
 
-        if (horizontalMovement == 0 && verticalMovement == 0)
+    private void OnDrawGizmos()
+    {
+        // score area gizmos
+        Gizmos.color = Color.cyan;
+        Gizmos.DrawWireSphere(GetPlayerCenter(), PlayerRadius);
+    }
+
+    // player controls
+    void MovePlayer()
+    {
+        if (HorizontalMovement == 0 && VerticalMovement == 0)
         {
             rb.velocity = new Vector2(0, 0);
             return;
         }
-        var movementInput = new Vector2(horizontalMovement, verticalMovement);
+        var movementInput = new Vector2(HorizontalMovement, VerticalMovement);
         var velo = movementInput * MovementSpeed;
-        rb.MovePosition((Vector2) transform.position + (velo * Time.deltaTime));
+        rb.MovePosition((Vector2)transform.position + (velo * Time.deltaTime));
     }
+
+    void DetectTrash(Vector3 center, float radius)
+    {
+        // detect score area
+        Collider[] colliders = Physics.OverlapSphere(center, radius);
+        Debug.Log($"Number of colliders: {colliders.Length}");
+    }
+
+    Vector3 GetPlayerCenter()
+    {
+        return transform.position;
+    }
+
+
 
     //private void OnCollisionEnter2D(Collision2D collision)
     //{
@@ -47,22 +74,5 @@ public class Player : MonoBehaviour
     //        scoreText.text = $"{GameManager.Instance.GetCurrentScore():D4}";
     //        Destroy(collision.gameObject);
     //    }
-    //}
-
-    //void DetectTrash(Vector3 center, float radius)
-    //{
-    //    Collider[] hitColliders = Physics.OverlapSphere(center, radius);
-    //    Debug.Log(hitColliders.Length);
-    //}
-
-    //Vector3 GetPlayerCenter()
-    //{
-    //    return gameObject.transform.position;
-    //}
-
-    //private void OnDrawGizmos()
-    //{
-    //    Gizmos.color = Color.cyan;
-    //    Gizmos.DrawWireSphere(GetPlayerCenter(), 1);
     //}
 }
