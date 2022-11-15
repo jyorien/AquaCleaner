00507b66e5627cb23cb31c791ecae9c74ce1a14e
Joey Lim <jyorin0206@gmail.com>
2022-11-15 21:01:12 +0800

    added pixel sprites and animation and changed player movement

--- Assets/Scripts/BeachPlayer.cs
+++ Assets/Scripts/BeachPlayer.cs
@@ -5,8 +5,10 @@ using UnityEngine;
 public class BeachPlayer : MonoBehaviour
 {
     [SerializeField] float MovementSpeed;
+    [SerializeField] Animator animator;
     float HorizontalMovement;
     float VerticalMovement;
+    bool left, right, forward, backward = false;
     Rigidbody2D rb;
     // Start is called before the first frame update
     void Start()
@@ -20,6 +22,28 @@ public class BeachPlayer : MonoBehaviour
     {
         HorizontalMovement = Input.GetAxisRaw("Horizontal");
         VerticalMovement = Input.GetAxisRaw("Vertical");
+        if ( Input.GetKeyDown(KeyCode.W))
+        {
+            backward = true;
+            left = right = forward = false;
+        }
+        else if (Input.GetKeyDown(KeyCode.A))
+        {
+            left = true;
+            forward = right = backward = false;
+        }
+        else if (Input.GetKeyDown(KeyCode.S))
+        {
+            forward = true;
+            left = right = backward   = false;
+        }
+        else if (Input.GetKeyDown(KeyCode.D))
+        {
+            right = true;
+            left = forward = backward = false;
+        }
+
+
     }
 
     private void FixedUpdate()
@@ -29,7 +53,22 @@ public class BeachPlayer : MonoBehaviour
 
     void MovePlayer()
     {
-        var movementInput = new Vector2(HorizontalMovement, VerticalMovement);
+        Vector2 movementInput = new Vector2(0,0);
+        if (left || right)
+        {
+            movementInput = new Vector2(HorizontalMovement, 0);
+            animator.SetFloat("Horizontal", HorizontalMovement);
+            animator.SetFloat("Vertical", 0);
+
+        }
+        else if (forward || backward)
+        {
+            movementInput = new Vector2(0, VerticalMovement);
+            animator.SetFloat("Vertical", VerticalMovement);
+            animator.SetFloat("Horizontal", 0);
+        }
+
+   
         var velo = movementInput * MovementSpeed;
         var newPosition = (Vector2)transform.position + (velo * Time.deltaTime);
         //if (!IsOutOfBounds(newPosition))
