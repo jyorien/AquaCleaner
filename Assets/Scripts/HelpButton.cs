110ae7946c97a1df315ae90b19825bfa19020cc4
Joey Lim <jyorin0206@gmail.com>
2022-12-05 15:32:43 +0800

    made instructions on hover

--- Assets/Scripts/HelpButton.cs
+++ Assets/Scripts/HelpButton.cs
@@ -2,30 +2,28 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
-public class HelpButton : MonoBehaviour
+using UnityEngine.EventSystems;
+public class HelpButton : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
 {
     [SerializeField] GameObject InstructionsMenu;
     bool isDisplayed = false;
+
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        InstructionsMenu.SetActive(true);
+    }
+
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        InstructionsMenu.SetActive(false);
+    }
+
     // Start is called before the first frame update
     void Start()
     {
         InstructionsMenu.SetActive(false);
-        Button btn = GetComponent<Button>();
-        btn.onClick.AddListener(() => {
-            isDisplayed = !isDisplayed;
-            if (isDisplayed)
-            {
-                InstructionsMenu.SetActive(true);
-            } else
-            {
-                InstructionsMenu.SetActive(false);
-            }
-        });
+
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+   
 }
